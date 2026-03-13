e19dcbc68c6134db4a6cc1df8ac05d7af50ecdc1
Yurcell <chestayurcel@gmail.com>
2026-03-13 23:27:03 +0700

    Initial commit

---
+++ src/main/java/com/example/tugascrud/TugascrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.tugascrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugascrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugascrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tugascrud/TugascrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tugascrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugascrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

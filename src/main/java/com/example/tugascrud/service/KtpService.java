8b5c4780e5f4ec1487666877cc7e4dfc42e5355c
Yurcell <chestayurcel@gmail.com>
2026-03-14 21:59:59 +0700

    membuat kodingan untuk 2 file yaitu KtpService.java & KtpServiceImpl.java

--- src/main/java/com/example/tugascrud/service/KtpService.java
+++ src/main/java/com/example/tugascrud/service/KtpService.java
@@ -1,4 +1,12 @@
 package com.example.tugascrud.service;
 
+import com.example.tugascrud.entity.Ktp;
+import java.util.List;
+
 public interface KtpService {
+    Ktp create(Ktp ktp);
+    List<Ktp> getAll();
+    Ktp getById(Integer id);
+    Ktp update(Integer id, Ktp ktp);
+    void delete(Integer id);
 }
--- src/main/java/com/example/tugascrud/service/impl/KtpServiceImpl.java
+++ src/main/java/com/example/tugascrud/service/impl/KtpServiceImpl.java
@@ -1,4 +1,52 @@
 package com.example.tugascrud.service.impl;
 
-public class KtpServiceImpl {
+import com.example.tugascrud.entity.Ktp;
+import com.example.tugascrud.repository.KtpRepository;
+import com.example.tugascrud.service.KtpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class KtpServiceImpl implements KtpService {
+
+    @Autowired
+    private KtpRepository repository;
+
+    @Override
+    public Ktp create(Ktp ktp) {
+        if (repository.existsByNomorKtp(ktp.getNomorKtp())) {
+            throw new RuntimeException("Nomor KTP sudah terdaftar!");
+        }
+        return repository.save(ktp);
+    }
+
+    @Override
+    public List<Ktp> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Ktp getById(Integer id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Data KTP tidak ditemukan"));
+    }
+
+    @Override
+    public Ktp update(Integer id, Ktp ktpDetails) {
+        Ktp ktp = getById(id);
+        if (repository.existsByNomorKtpAndIdNot(ktpDetails.getNomorKtp(), id)) {
+            throw new RuntimeException("Nomor KTP sudah dipakai orang lain!");
+        }
+        ktp.setNomorKtp(ktpDetails.getNomorKtp());
+        ktp.setNamaLengkap(ktpDetails.getNamaLengkap());
+        ktp.setAlamat(ktpDetails.getAlamat());
+        ktp.setTanggalLahir(ktpDetails.getTanggalLahir());
+        ktp.setJenisKelamin(ktpDetails.getJenisKelamin());
+        return repository.save(ktp);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        repository.delete(getById(id));
+    }
 }

864abb2552b3285f07591de72d1197de9d4fe2e0
Yurcell <chestayurcel@gmail.com>
2026-03-14 21:56:49 +0700

    membuat kodingan untuk 2 file yaitu KtpMapper.java & ResponseUtil.java

--- src/main/java/com/example/tugascrud/mapper/KtpMapper.java
+++ src/main/java/com/example/tugascrud/mapper/KtpMapper.java
@@ -1,4 +1,17 @@
 package com.example.tugascrud.mapper;
 
+import com.example.tugascrud.dto.KtpDto;
+import com.example.tugascrud.entity.Ktp;
+
 public class KtpMapper {
+    public static Ktp toEntity(KtpDto dto) {
+        Ktp ktp = new Ktp();
+        ktp.setId(dto.getId());
+        ktp.setNomorKtp(dto.getNomorKtp());
+        ktp.setNamaLengkap(dto.getNamaLengkap());
+        ktp.setAlamat(dto.getAlamat());
+        ktp.setTanggalLahir(dto.getTanggalLahir());
+        ktp.setJenisKelamin(dto.getJenisKelamin());
+        return ktp;
+    }
 }
--- src/main/java/com/example/tugascrud/util/ResponseUtil.java
+++ src/main/java/com/example/tugascrud/util/ResponseUtil.java
@@ -1,4 +1,14 @@
 package com.example.tugascrud.util;
 
+import com.example.tugascrud.model.WebResponse;
+import org.springframework.http.ResponseEntity;
+
 public class ResponseUtil {
+    public static <T> ResponseEntity<WebResponse<T>> build(String status, String message, T data, int code) {
+        WebResponse<T> res = new WebResponse<>();
+        res.setStatus(status);
+        res.setMessage(message);
+        res.setData(data);
+        return ResponseEntity.status(code).body(res);
+    }
 }
